e95aecf02231a79391968e19806b6b3cfd3226d7
Thiago <thiagofreitascarneiro@hotmail.com>
2022-02-28 18:27:47 -0300

    start project NodeJs

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const {v4: uuidv4 } = require("uuid") 
+
+const app = express();
+
+app.use(express.json())
+
+const customers = [];
+
+// * cpf - String
+// name - String
+// id - uuid
+// statement []
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const id = uuidv4();
+
+    customers.push({
+        cpf, 
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+})
+
+app.listen(3333);
