ac6c722e08d2dc2202e69e28bb4429ac87f3f05a
Thiago <thiagofreitascarneiro@hotmail.com>
2022-03-01 17:45:08 -0300

    must be possible to get bank statement

--- src/index.js
+++ src/index.js
@@ -12,6 +12,24 @@ const customers = [];
 // id - uuid
 // statement []
 
+
+//Middleware 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found" })
+    }
+
+    request.customer = customer;
+
+    return next();
+
+}
+
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
@@ -27,10 +45,36 @@ app.post("/account", (request, response) => {
         cpf, 
         name,
         id: uuidv4(),
-        statement: []
+        statement: [],
     });
 
     return response.status(201).send();
+});
+
+//app.use(verifyIfExistsAccountCPF)
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer.statement);
+})
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit',
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send()
 })
 
 app.listen(3333);
+
